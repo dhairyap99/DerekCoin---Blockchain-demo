7de2b0642bb2cdd81926f35fa7c583671312b484
Dhairya <dhairya.parekh@somaiya.edu>
2021-04-04 13:57:11 +0530

    Implementing proof-of-work

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const SHA256 = require('crypto-js/sha256');
-jhahahah
 
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
@@ -8,16 +7,27 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
        this.chain = [this.createGenesisBlock()]; 
+       this.difficulty = 5;
     }
 
     createGenesisBlock(){ //This is to mark the first block, needs to be created manually
@@ -30,7 +40,8 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash; // Need previous block's hash
-        newBlock.hash = newBlock.calculateHash(); // Need to create a hash for new block
+        //newBlock.hash = newBlock.calculateHash(); // Need to create a hash for new block
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -54,13 +65,18 @@ class Blockchain{
 
 //Demo time
 let derekCoin = new Blockchain();
+
+console.log('Mining Block 1...');
 derekCoin.addBlock(new Block(1, "04/04/2021", {amount: 11}));
+
+console.log('Mining Block 2...');
 derekCoin.addBlock(new Block(2, "06/04/2021", {amount: 21}));
 
-console.log('Is blockchain valid?' + derekCoin.isChainValid());
+// console.log('Is blockchain valid?' + derekCoin.isChainValid());
 
-derekCoin.chain[1].data = {amount: 100}; 
-derekCoin.chain[1].hash = derekCoin.chain[1].calculateHash();
+// derekCoin.chain[1].data = {amount: 100}; 
+// derekCoin.chain[1].hash = derekCoin.chain[1].calculateHash();
 
-console.log('Is blockchain valid?' + derekCoin.isChainValid());
-//console.log(JSON.stringify(derekCoin, null, 4));
+// console.log('Is blockchain valid?' + derekCoin.isChainValid());
+// console.log(JSON.stringify(derekCoin, null, 4));
+
